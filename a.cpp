e4a963af18229e137eeee550e4054b9748dba7b2
jarralKeshav <keshavjrall@outlook.com>
2023-07-14 19:30:11 +0530

    cses problemSet 1

--- a.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-cout<<"Hello World\n";
-cout<<10+19;
-return 0;
-}
---
+++ cses1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+long long  n;
+cin>>n;
+cout<<n<<" ";
+while(n!=1){
+if(n%2==0)n/=2;
+else n = n*3+1;
+cout<<n<<" ";
+}
+cout<<"\n";
+return 0;
+}

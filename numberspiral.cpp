abf7918a79a1b333a8d6362488f6b870509c1687
jarralKeshav <keshavjrall@outlook.com>
2023-08-02 01:35:06 +0530

    changed file names

--- numberspiral.cpp
+++ numberspiral.cpp
@@ -3,19 +3,20 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> pi;
 typedef vector<int> vi;
-int solve(int one,int two)
+int solve(int row,int col)
 {
- vector<vector<int>> vect
- {
-    {1,2,9,10,15},
-    {4,3,8,11,24},
-    {5,6,7,12,23},
-    {16,15,14,13,22},
-    {17,18,19,20,21}
- };
-
-
- int value = vect[one-1][two-1];
+    int value=0;
+ if(row>=col){
+    if(row%2==0){
+        value = pow(row-1,2)+(row-col)+row;
+    }
+    else{
+        value = pow(row-1,2)+col;
+    }
+ }
+ else{
+    
+ }
  return value;
  
 }
--- rough.cpp
+++ rough.cpp
@@ -1,20 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+#define nl "\n";
+// void solve()
+// {
+ 
+ 
+// }
 int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
-    
-    int x=1;
-    int n;
-    cin>>n;
-    int m=10;
-    for(int i=2;i<=n;i++)
-    {
-        x=(x*i)%m;
-        cout<<"x inside: "<<x<<"\n";
+  
+
+    int arr[]={-1,2,4,-3,5,2,-5,2};
+    int best=0,sum=0;
+    for(int i=0;i<8;i++){
+        sum = max(arr[i],sum+=arr[i]);
+        best = max(best,sum);
     }
-    cout<<x<<"\n";
+    cout<<best<<nl;
+ 
+    // solve();
+ 
+    
     return 0;
 }
